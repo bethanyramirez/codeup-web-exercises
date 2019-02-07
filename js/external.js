f32aef981e8ac613445f5841da50faeb2dc5f5a2
Bethany Ramirez <bethany.e.ramirez@gmail.com>
2019-02-07 10:31:36 -0600

    add external.js file

--- js/external.js
+++ js/external.js
@@ -18,7 +18,7 @@ var pricePerMovie = 3;
 
 var totalPrice = (Number(daysOfMermaid) + Number(daysOfBro) + Number(daysOfHercules)) * pricePerMovie;
 
-alert("Your total is $" + totalPrice );
+alert("Your total is $" + totalPrice.toFixed(2));
 
 
 /* Next Bullet */
@@ -59,11 +59,33 @@ var totalFacebookPay = facebookRate * facebookHours;
 
 var totalPaycheck = totalGooglePay + totalAmazonPay + totalFacebookPay;
 
-alert("Your total paycheck is " + totalPaycheck);
+alert("Your total paycheck is $" + totalPaycheck);
 
 
+/* 3rd bullet point */
+
+var classIsFull = confirm("Confirm is class is full");
+
+var ScheduleConflict = confirm("Confirm if there is a schedule conflict");
+
+if (classIsFull && !ScheduleConflict) {
+
+    console.log("You can enroll in class");
+
+}
 
 
+/* 4th bullet point */
+var username = "codeup";
 
+var password = "notastrongpassword";
 
+var passwordMinChar = true;
 
+var passwordContainsUsername = password.includes(username);
+
+var userNameLess20Char = username.length < 20;
+
+var trimmedUsername = username.trim();
+
+var trimmedPassword = password.trim();
